b9c08336f0ad0a58255269200fba081619bbe92d
Devendra Yadav <devendra.yadav@gmail.com>
2018-08-24 16:14:21 +0800

    First commit

---
+++ src/com/dev/Utility.java
@@ -0,0 +1,13 @@
+package com.dev;
+
+public class Utility {
+	
+	public static boolean isNull(Object theObject) {
+		if(theObject==null) {
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
+}
